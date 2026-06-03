84e094bc085cec3615a8af4f9e470bbd89f67c5e
Nino Kveseladze <nkveseladze@credo.ge>
2026-06-03 20:14:46 +0400

    [სახელი ნ.] HW5 — GradeCalculator

---
+++ src/main/java/homework5/GradeCalculator.java
@@ -0,0 +1,28 @@
+
+/*@author ნინო კვესელაძე*/
+package homework5;
+
+public class GradeCalculator { public static String getGrade(int score) {
+
+    if (score < 0 || score > 100) {
+        return "Invalid";
+    } else if (score >= 90) {
+        return "A";
+    } else if (score >= 80) {
+        return "B";
+    } else if (score >= 70) {
+        return "C";
+    } else if (score >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+}
+
+    public static void main(String[] args) {
+
+        int myScore = 13;
+
+        System.out.println(getGrade(myScore));
+    }
+}
