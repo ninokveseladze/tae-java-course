367dd00eb2657753445e8fe3620bb14a65e127eb
Nino Kveseladze <nkveseladze@credo.ge>
2026-06-07 01:36:44 +0400

    [Nino K.] HW6-BankAccount

--- src/main/java/homework6/Main.java
+++ src/main/java/homework6/Main.java
@@ -1,10 +1,25 @@
+/*@author-Nino Kveseladze*/
 package homework6;
-
-public class Runner {
+public class Main {
     public static void main (String[] args){
+        //დავალება#1
+
         Person person1 = new Person("Nino",30,"kveseladze.nino@gmail.com");
         Person person2= new Person("რომა",23,"roma@gmail.com");
         System.out.println(person1.getName());
         person2.introduce();
+
+        //დავალება#2
+
+        BankAccount acc = new BankAccount("Nino",100);
+        acc.deposit(50);
+        System.out.println(acc.getBalance());
     }
+
+
 }
+
+
+
+
+
