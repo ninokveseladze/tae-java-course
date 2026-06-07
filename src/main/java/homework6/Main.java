8b799564ecdc78b16eb8f3c83fd8bac341f1c7ef
Nino Kveseladze <nkveseladze@credo.ge>
2026-06-07 13:20:34 +0400

    [Nino K.] HW6-Counter

--- src/main/java/homework6/Main.java
+++ src/main/java/homework6/Main.java
@@ -14,6 +14,16 @@ public class Main {
         BankAccount acc = new BankAccount("Nino",100);
         acc.deposit(50);
         System.out.println(acc.getBalance());
+
+        //დავალება#3
+
+        Counter c = new Counter();
+        c.countTo(5);
+        System.out.println(c.count);
+
+
+
+
     }
 
 
