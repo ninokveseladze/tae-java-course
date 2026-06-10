68f136759e6c48fb9cca7f7599db2284f66983b4
Nino Kveseladze <nkveseladze@credo.ge>
2026-06-10 22:25:29 +0400

    [Nino K.] HW7 — Person

---
+++ src/main/java/homework7/Person.java
@@ -0,0 +1,23 @@
+/*Author-Nino Kveseladze*/
+package homework7;
+
+public class Person implements Comparable<Person> {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return Integer.compare(this.age, other.age);
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + age;
+    }
+}
