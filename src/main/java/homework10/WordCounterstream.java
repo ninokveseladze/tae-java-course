e1b3bd6226e81ab37070a1dc0ee6992cb670a32f
Nino Kveseladze <nkveseladze@credo.ge>
2026-06-24 01:50:11 +0400

    [Nino k.] HW10 — WordCounterstream

---
+++ src/main/java/homework10/WordCounterstream.java
@@ -0,0 +1,21 @@
+
+/*Author-Nino Kveseladze*/
+package homework10;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+public class WordCounterstream { public static void main(String[] args) {
+
+    String text = "java git java oop java git python";
+
+    String[] words = text.split(" ");
+
+    Map<String, Long> counts = Arrays.stream(words)
+            .collect(Collectors.groupingBy(
+                    w -> w,
+                    Collectors.counting()
+            ));
+
+    System.out.println(counts);
+}
+}
